d1dd895458f6005791071aad21b126ff6c974376
Visvaldas <visva.rapalis@gmail.com>
2021-01-13 17:52:52 +0200

    Add monsters img and email & style cards

--- src/App.js
+++ src/App.js
@@ -9,26 +9,22 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+    };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
-      <div className="App">
-      <CardList name='Visva'>
-      { this.state.monsters.map(monster => (
-        <h1 key={monster.id}> {monster.name} </h1>
-      )) }
-      </CardList>
-    </div>
-    )
+      <div className='App'>
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
   }
 }
 
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 
 import './card-list.styles.css';
 
-export const CardList = props => {
-  console.log(props);
-  return <div className='card-list'>{props.children}</div>;
-}
+export const CardList = (props) => (
+  <div className='card-list'>
+    {props.monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
