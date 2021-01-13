9dbdd0a560af3dc28b7f644385219900be12d704
Visvaldas <visva.rapalis@gmail.com>
2021-01-13 17:05:48 +0200

    Add real monsters data from api and style it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <CardList name='Visva'>
+      { this.state.monsters.map(monster => (
+        <h1 key={monster.id}> {monster.name} </h1>
+      )) }
+      </CardList>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
